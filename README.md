6e0e8ca2920f35528f43304d36dbf468356d6e2b
unknown <samip0884@gmail.com>
2020-08-27 13:35:31 +0545

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-Project Management System
+# Project_Manager
+A project manager application
+
+**Steps to start Project Manager**
+1. `\i 'path_to_base_folder/dbSetup.sql` in PostgreSQL terminal inside the prefered database.
+2. `yarn` or `npm' 'start` in PMSserver folder.
+3. `yarn` or `npm' 'start` in PMSfrontend folder.
+4. Go to `localhost:3000/dashboard`.
+
+**Requirements**
+1. NodeJS <br>
+**Modules Needed**<br>
+express <br>
+dotenv <br>
+express-validator 5.3.1 <br>
+body-parser <br>
+jsonwebtoken <br>
+2. PostgreSQL
+3. ReactJS <br>
+**Modules Needed** <br>
+react-redux <br>
+redux-thunk <br>
--- dbSetup.sql
+++ dbSetup.sql
@@ -8,20 +8,25 @@ CREATE TABLE employees (
 	role VARCHAR(50) NOT NULL,
 	salt VARCHAR(100) NOT NULL);
 
+INSERT into employees (emp_name, password, email, role, salt)
+VALUES
+('Admin','bc9b406a98bc4fe7ac6970e1065e65a6141a28422d65a4a4e3925f89a1bdbe1845cca651c2a10754a89cc5769f7d8e0bad54a09d6cc24afc911964d447895aa5','admin@lf.com','admin','0b54393165d91a3c');
+
+
 DROP TABLE IF EXISTS projects;
 
 CREATE TABLE projects(
 	project_id SERIAL NOT NULL PRIMARY KEY,
 	title VARCHAR(100) NOT NULL UNIQUE,
 	description VARCHAR(500) NOT NULL,
-	project_manager INT NOT NULL REFERENCES employees.emp_id);
+	project_manager INT NOT NULL REFERENCES employees(emp_id));
 
 
 DROP TABLE IF EXISTS employee_projects;
 
 CREATE TABLE employee_projects(
-	emp_id INT NOT NULL REFERENCES employees.emp_id,
-	project_id INT NOT NULL REFERENCES projects.project_id);
+	emp_id INT NOT NULL REFERENCES employees(emp_id),
+	project_id INT NOT NULL REFERENCES projects(project_id));
 
 DROP TABLE IF EXISTS tasks;
 
@@ -29,25 +34,25 @@ CREATE TABLE tasks(
 	task_id SERIAL NOT NULL PRIMARY KEY,
 	title VARCHAR(100) NOT NULL UNIQUE,
 	description VARCHAR(500) NOT NULL,
-	assignee INT REFERENCES employees.emp_id,
-	project INT NOT NULL REFERENCES projects.project_id,
+	assignee INT REFERENCES employees(emp_id),
+	project INT NOT NULL REFERENCES projects(project_id),
 	deadline DATE NOT NULL
 );
 
 
 DROP TABLE IF EXISTS task_tags;
 
-CREATE TABLE employee_projects(
-	emp_id INT NOT NULL REFERENCES employees.emp_id,
-	task_id INT NOT NULL REFERENCES tasks.task_id);
+CREATE TABLE task_tags(
+	emp_id INT NOT NULL REFERENCES employees(emp_id),
+	task_id INT NOT NULL REFERENCES tasks(task_id));
 
 DROP TABLE IF EXISTS comments;
 
-CREATE TABLE projects(
+CREATE TABLE comments(
 	comment_id SERIAL NOT NULL PRIMARY KEY,
 	comment VARCHAR(500) NOT NULL,
-	commenter INT NOT NULL REFERENCES employees.emp_id,
-	task INT NOT NULL REFERENCES task.task_id);
+	commenter INT NOT NULL REFERENCES employees(emp_id),
+	task INT NOT NULL REFERENCES tasks(task_id));
 
 
 
